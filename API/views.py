0ffa7e414355ce8d2d62d75298c31619f1a17858
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-04-20 17:47:23 +0430

    create course view

--- API/views.py
+++ API/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CourseSerializers
+from .models import Course
+
 
 # Create your views here.
+class CourseView(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializers
+

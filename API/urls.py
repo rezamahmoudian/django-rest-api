e93e630ff8a18417a15d1de1bb8e7b32d7221443
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-05-23 01:20:54 +0430

    add view and template and set url for articles

--- API/urls.py
+++ API/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import CourseViewSet, UserViewSet
 from rest_framework_simplejwt import views as jwt_views
+from .views import ArticleListView
 
 
 router = routers.DefaultRouter()
@@ -12,4 +13,5 @@ urlpatterns = [
     path('v1/', include(router.urls)),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', ArticleListView.as_view(), name='home'),
 ]
--- API/views.py
+++ API/views.py
@@ -4,6 +4,8 @@ from .serializers import CourseSerializers, UserSerializers
 from .models import Course
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from django.views.generic import ListView
+from .models import Article
 
 
 # Create your views here.
@@ -16,4 +18,10 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializers
     permission_classes = [permissions.IsAdminUser]
+
+
+class ArticleListView(ListView):
+    template_name = './blog/article_list.html'
+    def get_queryset(self):
+        return Article.objects.all()
 

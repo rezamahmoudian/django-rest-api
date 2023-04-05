1d219119e17e390d6fe30ad8d3f804c500b5d9c9
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-04-05 10:55:18 +0430

    add nested serializer

--- API/serializers.py
+++ API/serializers.py
@@ -3,6 +3,13 @@ from .models import Course, Article
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
+
+class AuthorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "first_name", "last_name", "email")
+
+
 class CourseSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
@@ -12,19 +19,12 @@ class CourseSerializers(serializers.HyperlinkedModelSerializer):
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields =(
-            "id",
-            "username",
-            "first_name",
-            "last_name",
-            "email",
-            "is_staff",
-            "is_active",
-            "is_superuser",
-        )
+        fields = '__all__'
 
 
 class ArticleSerializers(serializers.ModelSerializer):
+    author = AuthorSerializers()
+
     class Meta:
         model = Article
         # fields = '__all__'
